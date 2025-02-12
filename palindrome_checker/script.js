fc28fd8bd07449f97a4d91c143acb73918bcbee7
Henrique Sousa <henriquesousa@ymail.com>
2025-02-12 11:00:59 -0300

    finished script

--- palindrome_checker/script.js
+++ palindrome_checker/script.js
@@ -1,16 +1,28 @@
 const checkButton = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+
 const empty = () => {
-    if(document.getElementById('text-input').value === '') {
+    const textInput = document.getElementById('text-input').value;
+    const text = textInput.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, "");
+    if(text === '') {
+        result.classList.add('hidden');
         return alert('Please input a value');
     }
 }    
 const checkPalindrome = () => {
-    empty();
-    const text = document.getElementById('text-input').value;
+    const textInput = document.getElementById('text-input').value;
+    const text = textInput.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
     const textReversed = text.split('').reverse().join('');
-    if(text === textReversed) {
-        return alert('It is a palindrome');
+
+    if(text === textReversed && text !== '') {
+        result.classList.remove('hidden');
+        result.textContent = `${textInput} is a palindrome`;
     }
-    return alert('It is not a palindrome');
-}
+    else if(text !== textReversed){
+        result.classList.remove('hidden');
+        result.textContent = `${textInput} is not a palindrome`;}
+    else{
+        empty();
+    }}
 checkButton.addEventListener('click', checkPalindrome);
