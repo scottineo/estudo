ea188ea482e1622e30a97f2b63910aec59a8c42b
Henrique Sousa <henriquesousa@ymail.com>
2025-02-12 12:31:48 -0300

    title animation

--- palindrome_checker/script.js
+++ palindrome_checker/script.js
@@ -1,7 +1,7 @@
 const checkButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
-
+// Function to check if the input is empty
 const empty = () => {
     const textInput = document.getElementById('text-input').value;
     const text = textInput.toLowerCase().replace(/[^a-zA-Z0-9\s]/g, "");
@@ -9,7 +9,8 @@ const empty = () => {
         result.classList.add('hidden');
         return alert('Please input a value');
     }
-}    
+}
+// Function to check if the word is a palindrome
 const checkPalindrome = () => {
     const textInput = document.getElementById('text-input').value;
     const text = textInput.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
@@ -26,3 +27,29 @@ const checkPalindrome = () => {
         empty();
     }}
 checkButton.addEventListener('click', checkPalindrome);
+
+//Function to animate the title
+const animateTitle = () => {
+    const title = document.getElementById('title');
+    const titleText = title.textContent;
+    console.log(titleText.length);
+
+    title.innerHTML = "";
+  
+    for (let i = 0; i < titleText.length; i++) {
+      const letterSpan = document.createElement('span');
+      letterSpan.textContent = titleText[i];
+      title.appendChild(letterSpan);
+    }
+  
+    const letters = document.querySelectorAll('span');
+
+    letters.forEach((letter, idx) => {
+      letter.style.animationDelay = `${idx * 50}ms`;
+    }
+);
+letters[1].style.width = "7px";
+letters[4].style.width = "7px";
+
+  }
+    animateTitle();
